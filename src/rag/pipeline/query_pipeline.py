430972912c2dfd15570448b740d34b458971104a
Ediomoabasi Udoaka <counseleudoakar@gmail.com>
2026-04-19 22:44:22 +0100

    Fixed ModuleNotFound Error on transformation file for RAG agent

--- src/rag/pipeline/query_pipeline.py
+++ src/rag/pipeline/query_pipeline.py
@@ -1,7 +1,7 @@
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain.chains import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 import os
 
