c983c53046b1dc1c762b10c5bae219685f1890ec
Ediomoabasi Udoaka <counseleudoakar@gmail.com>
2026-04-19 22:16:41 +0100

    Fixed ModuleNotFpund Error on transformation file for RAG agent

--- src/rag/pipeline/query_pipeline.py
+++ src/rag/pipeline/query_pipeline.py
@@ -1,6 +1,6 @@
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 import os
